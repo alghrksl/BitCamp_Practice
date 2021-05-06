801af02654a98ab41db616136a1d705db960e5b1
김훈 <50814859+alghrksl@users.noreply.github.com>
2021-05-06 10:46:12 +0900

    Samplemultiplication

---
+++ Sample01/src/sample01/Sample05.java
@@ -0,0 +1,27 @@
+package sample01;
+
+public class Sample05 {
+
+	public static void main(String[] args) {
+
+//		for (int i = 0; i <= 10; i++) {
+//
+//			System.out.println(i);
+//
+//		}
+		for (int j = 1; j <= 9; j++) {
+			
+			System.out.println(j +"´Ü °ö¼À");
+			System.out.println();
+			
+			for (int k = 1; k <= 9; k++) {
+
+				System.out.println(k +" * " + j + " = " + k*j);
+
+			}
+			System.out.println();
+		}
+
+	}
+
+}
