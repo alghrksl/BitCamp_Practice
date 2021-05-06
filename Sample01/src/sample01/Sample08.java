420bff4b8e505074905b25ffbc3224fbd773aca8
김훈 <50814859+alghrksl@users.noreply.github.com>
2021-05-06 12:54:09 +0900

    SampleDowhileContinue

---
+++ Sample01/src/sample01/Sample08.java
@@ -0,0 +1,42 @@
+package sample01;
+
+import java.util.Iterator;
+
+public class Sample08 {
+
+	public static void main(String[] args) {
+//		int a = 10;
+//		
+//		do {
+//			System.out.println(a);
+//			a++;
+//			System.out.println();
+//		}while (a <= 20);
+
+//		for (int i = 1; i <= 10; i++) {
+//			if (i == 2) {
+//				break;
+//			}
+//			System.out.println(i);
+//		}
+
+//		int j = 0;
+//		while (j <= 10) {
+//			if(j == 2) {
+//				break;
+//			}
+//			
+//			System.out.println(j);
+//			j++;
+//		}
+		
+		
+		for(int num = 1; num <= 10; num++) {
+			if ( num % 2 != 0) {
+				continue;
+			}
+			System.out.println(num);
+		}
+	}
+
+}
