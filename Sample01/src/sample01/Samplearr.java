ef48be33c2e8c2261729847953df84d094122ca4
김훈 <50814859+alghrksl@users.noreply.github.com>
2021-05-07 10:16:18 +0900

    Sample hard 2-2

--- Sample01/src/sample01/Samplearr.java
+++ Sample01/src/sample01/Samplearr.java
@@ -1,30 +1,40 @@
 package sample01;
 
+import java.util.Iterator;
+
 public class Samplearr {
 
 	public static void main(String[] args) {
-	
-		String A[]= new String[7];
+
 		String[][] student = { { "A", "B", "A", "C", "C", "D", "E", "F", "A", "D" },
 				{ "D", "B", "A", "B", "C", "A", "E", "F", "A", "D" },
 				{ "E", "D", "D", "A", "C", "B", "E", "E", "A", "D" },
 				{ "E", "D", "D", "A", "C", "B", "E", "E", "A", "D" },
-				{ "A", "B", "D", "C", "C", "D", "E", "E", "A", "D" },
-				{ "D", "B", "D", "C", "C", "D", "A", "E", "A", "D" } };
+				{ "A", "B", "D", "C", "C", "D", "E", "E", "A", "D" }, };
 
-		for (int i = 0; i <= 6; i++) {
+		String[] answer = { "D", "B", "D", "C", "C", "D", "A", "E", "A", "D" };
 
+		int count = 0;
+
+		for (int i = 0; i < student.length; i++) {
+			System.out.print(student[i][9]);
+			for (int j = 0; j < student[i].length; j++) {
+				System.out.print(student[i][j]);
 				
-				A[i] = student[i][9];
-				System.out.print(A[i]);
 				
+				if (student[i][j] == answer[j]) {  /// 배열 비교
+
+					count++; ///////////// 카운팅
+
+				}
+
 			}
 
-			
-			
+			System.out.println();
+			System.out.printf("채점 결과 : %d", count);
+			System.out.println();
 		}
 
-		
 	}
 
-
+}
