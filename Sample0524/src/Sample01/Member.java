0f7417729b22f84104a00561b1a02e572ee7717a
김훈 <50814859+alghrksl@users.noreply.github.com>
2021-05-24 18:08:34 +0900

    Update Person

--- Sample0524/src/Sample01/Member.java
+++ Sample0524/src/Sample01/Member.java
@@ -8,35 +8,50 @@ public class Member {
 		int firstnum, secondNum;
 		String str;
 
-		Scanner scanner = new Scanner(System.in);
+		double height = 170;
+		int age = 20;
+		String name = "Kimhoon";
+		String book = "Java의 정석";
+		String yn;
+		boolean hasbook = (book == "Java의 정석");
 
-		System.out.print("첫째 값 입력  : ");
-		firstnum = scanner.nextInt();
-		System.out.print("둘째 값 입력  : ");
-		secondNum = scanner.nextInt();
-		System.out.print("수식 입력 : ");
-		str = scanner.next();
+		if (hasbook == true) {
+			yn = "Yes";
+			System.out.printf("나이 %s 이름 %s 책여부 %s 키 %s", age, name, yn, height);
+		} else {
+			yn = "No";
+			System.out.printf("나이 %s 이름 %s 책여부 %s 키 %s", age, name, yn, height);
+		}
 
-		Calculater Cal = new Calculater();
-
-		switch (str) {
-		case "+": {
-			Cal.add(firstnum, secondNum);
-			break;
-		}
-		case "-": {
-			Cal.sub(firstnum, secondNum);
-			break;
-		}
-		case "/": {
-			Cal.divide(firstnum, secondNum);
-			break;
-		}
-		case "*": {
-			Cal.mul(firstnum, secondNum);
-			break;
-		}
-		}
+//		Scanner scanner = new Scanner(System.in);
+//
+//		System.out.print("첫째 값 입력  : ");
+//		firstnum = scanner.nextInt();
+//		System.out.print("둘째 값 입력  : ");
+//		secondNum = scanner.nextInt();
+//		System.out.print("수식 입력 : ");
+//		str = scanner.next();
+//
+//		Calculater Cal = new Calculater();
+//
+//		switch (str) {
+//		case "+": {
+//			Cal.add(firstnum, secondNum);
+//			break;
+//		}
+//		case "-": {
+//			Cal.sub(firstnum, secondNum);
+//			break;
+//		}
+//		case "/": {
+//			Cal.divide(firstnum, secondNum);
+//			break;
+//		}
+//		case "*": {
+//			Cal.mul(firstnum, secondNum);
+//			break;
+//		}
+//		}
 
 	}
 }
---
+++ Sample0524/src/Sample01/Person.java
@@ -0,0 +1,16 @@
+package Sample01;
+
+public class Person {
+
+	public static void main(String[] args) {
+		String name = "Kimhoon";
+		String phone_number = "010-3753-0532";
+		String person_number = "960430-1211432";
+		
+		System.out.println(name);
+		System.out.println(phone_number);
+		System.out.println(person_number);
+
+	}
+
+}
