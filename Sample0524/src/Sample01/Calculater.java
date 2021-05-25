b470c2f8bd40fa161d79c4bbae4ed3df5efda1a7
김훈 <50814859+alghrksl@users.noreply.github.com>
2021-05-25 12:09:05 +0900

    Change Cal

--- Sample0524/src/Sample01/Calculater.java
+++ Sample0524/src/Sample01/Calculater.java
@@ -14,21 +14,10 @@ class Calculater {
 		System.out.println(a);
 	}
 
-	void add(int a, int b) {
-		System.out.println(a + b);
-	}
-
-	void sub(int a, int b) {
-		System.out.println(a + b);
-	}
-
-	void mul(int a, int b) {
-		System.out.println(a + b);
-	}
-
-	void divide(int a, int b) {
-		System.out.println(a + b);
-	}
+	void add(int a, int b) { System.out.println(a + b); }
+	void sub(int a, int b) { System.out.println(a + b); }
+	void mul(int a, int b) { System.out.println(a + b); }
+	void divide(int a, int b) { System.out.println(a + b); }
 
 	public static void main(String[] args) {
 		String str;
@@ -84,7 +73,9 @@ class Calculater {
 			Calculater.circumferencea(firstnum);
 			break;
 		}
+		
 		}
 
 	}
+	
 }
