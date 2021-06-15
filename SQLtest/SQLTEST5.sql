3f1b66c7e89422421c00ad13cfb4e59824867d5d
김훈 <50814859+alghrksl@users.noreply.github.com>
2021-06-15 18:20:38 +0900

    Update SQLTEST5

--- SQLtest/SQLTEST5.sql
+++ SQLtest/SQLTEST5.sql
@@ -8,7 +8,7 @@ idx NUMBER(6) CONSTRAINT phoneinfo_basic_idx_pk PRIMARY KEY,
 fr_name VARCHAR2(20) NOT NULL,
 fr_phonenumber VARCHAR2(20) NOT NULL,
 fr_email VARCHAR2(20),
-fr-_address VARCHAR2(20),
+fr_address VARCHAR2(20),
 fr_regdate DATE DEFAULT sysdate
 );
 
@@ -16,9 +16,9 @@ CREATE TABLE  phoneinfo_univ(
 idx NUMBER(6) CONSTRAINT phoneinfo_univ_idx_pk PRIMARY KEY,
 fr_u_major VARCHAR2(20) DEFAULT 'N' 
                         NOT NULL,
-fr_u_number(1) DEFAULT '1' 
-               NOT NULL 
-              CONSTRAINT phoneinfo_univ_year_check CHECK(0 < fr_u_year AND fr_u_year < 5),
+fr_u_year NUMBER(1) DEFAULT '1' 
+                    NOT NULL 
+                    CONSTRAINT phoneinfo_univ_year_check CHECK(0 < fr_u_year AND fr_u_year < 5),
 fr_ref NUMBER(7) CONSTRAINT phoneinfo_univ_ref_fk REFERENCES phoneinfo_basic(idx) 
                  NOT NULL
 );
